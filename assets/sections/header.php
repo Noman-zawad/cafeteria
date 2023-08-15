9bfc86e4d12badb1b3ad78b72788075a61b683fb
Noman Zawad <nomanzawad@gmail.com>
2023-08-15 18:25:25 +0600

    started project

---
+++ assets/sections/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Zawad & Co</title>
+  <link rel="stylesheet" href="./assets/css/styles.css" />
+  <link rel="icon" type="image/x-icon" href="./assets/images/fav.png">
+</head>
+
+<body>
+  <header>
+    <div class="nav-wrapper">
+      <nav>
+        <div class="navbar">
+          <div class="left-nav">
+            <ul class="nav-menu">
+              <li class="nav-items current">
+                <a href="">Menu</a>
+              </li>
+            </ul>
+          </div>
+          <div class="center-nav">
+            <img src="./assets/images/logo.png" alt="Logo" class="logo">
+          </div>
+          <div class="right-nav">
+            <ul class="nav-menu">
+              <li class="nav-items current">
+                <a href="">Order Now</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include('./assets/sections/header.php'); ?>
+<section id="banner wrapper">
+    <div class="container">
+        <img src="./assets/images/logo.png" alt="">
+        <h1>WE SERVE QUALITY FOOD</h1>
+        <div class="divider"></div>
+        <div class="divider"></div>
+        <p class="banner-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever 1500s.
+        </p>
+        <button class="btn">ORDER NOW</button>
+    </div>
+</section>
+<?php include('./assets/sections/footer.php'); ?>
